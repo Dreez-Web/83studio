8fed5bab4f4641b302d184ff2c824bb814a718a2
Dreez-Web <alpuis.andres@gmail.com>
2025-06-12 10:39:56 -0400

    Agregando fuente Onest

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}',
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss-animated'), // <--- Asegúrate de que esta línea esté aquí
+    // Otros plugins si tienes
+  ],
+}
